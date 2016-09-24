7506513af81c265af50ae19f1a20159e7c56fa1f
ariiyu <yusuke.ary@gmail.com>
2016-09-24 13:56:50 +0900

    Initial Commit

---
+++ SpotifyPlayerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyPlayerSample
+//
+//  Created by Yusuke Ariyoshi on 2016/09/24.
+//  Copyright © 2016年 ariiyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
